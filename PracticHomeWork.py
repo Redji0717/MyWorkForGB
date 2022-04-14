e530a66ceab9fd648b2ec69a6f4ce18cabe1e460
Redji <ambosador4@gmail.com>
2022-04-14 14:13:23 +0300

    Practic Homework

---
+++ PracticHomeWork.py
@@ -0,0 +1,43 @@
+import re
+
+def email_parse(email_address):
+    parsed = re.findall(r"([^@&]+)@([\w_-][\w_\.-]*\.[\w_-]{2,})$", email_address)
+    if not parsed:
+        raise ValueError(f"wrong email: {email_address}")
+    return dict(zip(["username", "domain"], parsed[0]))
+
+
+from functools import wraps
+
+def type_logger(level=0):
+    def logger(func):
+        @wraps(func)
+        def decor(*argv):
+            if level > 0:
+                return 'calc_cube(' + ", ".join([f"{func(x)}:{type(func(x))}" for x in argv]) + ")"
+            else:
+                return "calc_cube(" + ", ".join([str(func(x)) for x in argv]) + ")"
+                return decor
+                return logger
+
+
+@type_logger(2)
+def calc_cube(x):
+    return x ** 3
+
+
+from functools import wraps
+
+def val_checker(func_filter):
+    def checker(func):
+        @wraps(func)
+        def decor(x):
+            if func_filter(x):
+                return func(x)
+            raise ValueError from ValueError
+            return decor
+            return checker
+
+@val_checker(lambda x: x > 0)
+def calc_cube(x):
+    return x ** 3
